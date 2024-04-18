b03013af00227a3c87a4f7d2db7956258848fad8
saatej <starantilia.9@gmail.com>
2024-04-18 16:16:47 +0530

    01-hello-world.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,9 @@
-this is again sample script
+#!/bin/bash
+
+
+
+echo "x:: Hello y, how are you?"
+echo "y:: Hi x, I am fine. How are you?"
+echo "x:: I am fine too. how is your work?"
+echo "y:: Now I am working as Devops engineer"
+ 
