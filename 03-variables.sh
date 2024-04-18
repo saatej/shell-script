27879d07aea1af35c4c23da56c9b42e5e22124f9
saatej <starantilia.9@gmail.com>
2024-04-18 20:47:44 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Anu
+PERSON2=Manu
+
+
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: Now I am working as Devops engineer"
