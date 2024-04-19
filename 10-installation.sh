7ee3b6125b7517cdc31d54d4f44ea78d11141ef2
saatej <starantilia.9@gmail.com>
2024-04-19 18:30:53 +0530

    shell-script

---
+++ 10-installation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+
+then
+
+echo "please run this script with root access"
+
+exit 1
+
+else
+
+echo "you are super user."
+
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+
+then
+
+echo "installation of mysql..failure"
+
+exit 1
+
+else 
+
+echo "installation of mysql...success"
+
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+
+then
+
+echo "installation of git..failure"
+
+exit 1
+
+else 
+
+installation of git...success
+
+fi
+
+echo "is script proceeding?"
+
