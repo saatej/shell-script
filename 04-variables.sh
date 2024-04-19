f49fd919ffb97884e68bb3c00623dd89b6930970
saatej <starantilia.9@gmail.com>
2024-04-19 11:53:59 +0530

    shell-script

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+
+PERSON1=$1
+PERSON2=$2
+
+
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: Now I am working as Devops engineer"
+
