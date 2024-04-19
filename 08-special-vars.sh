8f79c3fcd31de3453a2c935c7deac91136c06924
saatej <starantilia.9@gmail.com>
2024-04-19 14:25:18 +0530

    shell-script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,6 +7,6 @@ echo "number of variables passed: $#"
 
 echo "script name: $0"
 
-echo "current working directory: $pwd"
+echo "current working directory: $PWD"
 
 echo  "home directory of current user: $HOME"
