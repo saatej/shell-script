e3e2ac62ea47fc119c65910dfd73a4cc1002ca54
saatej <starantilia.9@gmail.com>
2024-04-19 23:04:55 +0530

    shell-script

---
+++ 11-functions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+validate(){
+    echo "exit status: $1"
+    echo "what are you doing: $2"
+
+}
+
+if [ $userid -ne 0]
+
+then
+
+echo "please run this script with root access."
+
+exit 1
+
+else 
+
+echo "you are super user."
+
+fi
+
+dnf install mysql -y
+
+validate
+
+if [ $? -ne 0 ]
+
+then
+
+echo "installation of mysql...failure"
+
+exit 1
+
+else
+
+echo "installation of mysql..success"
+
+fi
+
